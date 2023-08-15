eed79fcbad29b05e1e941e8c46719044628e2602
Sujeet Kumar Gupta <119527249+Suj7142@users.noreply.github.com>
2023-08-15 18:22:18 +0530

    Add files via upload

---
+++ Tour Buddy/about.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Travel Agency :: Best Agency</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+   <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+   <script>
+      $(document).ready(function(){
+          $(".scroll-top").click(function() {
+              $("html, body").animate({ 
+                  scrollTop: 0 
+              }, "slow");
+              return false;
+          });
+      });
+   </script>
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+
+<section class="header">
+
+<a href="home.php" class="logo"><img src="images/logo.png" alt="" style="height: 40px; width:200px;"></a>
+
+   <nav class="navbar">
+      <a href="home.php">home</a>
+      <a href="about.php" class="active">about</a>
+      <a href="package.php">package</a>
+      <a href="book.php">book</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+<div class="heading" style="background:url(images/header-bg-1.png) no-repeat">
+   <h1>about us</h1>
+</div>
+
+<!-- about section starts  -->
+
+<section class="about">
+
+   <div class="image">
+      <img src="images/about-pic.jpg" alt="">
+   </div>
+
+   <div class="content">
+      <h3>Why choose us?</h3>
+      <p style="font-size: 20px;" >Our TOUR BUDDY Is A Website That Provides Trip Fares, Or A Combination Of Both. It Gives Brief About The Famous Tourist Places Of INDIA.We provide Convenience to user During his/her travel.</p>
+      
+      <div class="icons-container" style="cursor:pointer">
+         <div class="icons">
+            <i class="fas fa-map"></i>
+            <span>top destinations</span>
+         </div>
+         <div class="icons" style="cursor:pointer">
+            <i class="fas fa-hand-holding-usd"></i>
+            <span>affordable price</span>
+         </div>
+         <div class="icons" style="cursor:pointer">
+            <i class="fas fa-headset"></i>
+            <span>24/7 guide service</span>
+         </div>
+      </div>
+   </div>
+
+</section>
+
+<!-- about section ends -->
+
+
+<!-- footer section starts  -->
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+         <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+      </div>
+
+   
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> 9999999999 </a>
+         <a href="#"> <i class="fas fa-phone"></i> 1234567890 </a>
+         <a href="#"> <i class="fas fa-phone"></i> 7247251454 </a>
+         <a href="#"> <i class="fas fa-envelope"></i> Tourbuddy@gmail.com </a>
+         
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href=""> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#" target="_blank"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#" target="_blank"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="https://www.linkedin.com/in/sujeet-gupta-27bb84223/" target="_blank"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <div class="credit"> created by <span>sujeet</span>  </div>
+
+</section>
+<!-- footer section ends -->
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
---
+++ Tour Buddy/home.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Travel Agency :: Best Agency</title>
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+   <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+   <script>
+      $(document).ready(function(){
+          $(".scroll-top").click(function() {
+              $("html, body").animate({ 
+                  scrollTop: 0 
+              }, "slow");
+              return false;
+          });
+      });
+   </script>
+</head>
+<body>
+<!-- header section starts  -->
+<section class="header">
+   <a href="home.php" class="logo"><img src="images/logo.png" alt="" style="height: 40px; width:200px;"></a>
+   <nav class="navbar">
+      <a href="home.php" class="active">home</a>
+      <a href="about.php">about</a>
+      <a href="package.php">package</a>
+      <a href="book.php">book</a>
+   </nav>
+   <div id="menu-btn" class="fas fa-bars"></div>
+</section>
+<!-- header section ends -->
+<!-- home section starts  -->
+<section class="home">
+
+   <div class="swiper home-slider">
+
+      <div class="swiper-wrapper">
+
+         <div class="swiper-slide slide" style="background:url(images/slide-1.jpg) no-repeat">
+            <div class="content">
+               <span>work, travel, save, repeat</span>
+               <h3>Have stories to tell not stuff to show</h3>
+               <a href="package.php" class="btn">discover more</a>
+            </div>
+         </div>
+
+         <div class="swiper-slide slide" style="background:url(images/jalmahal-slide-2.jpg) no-repeat">
+            <div class="content">
+               <span>explore, discover, travel</span>
+               <h3>discover the new places</h3>
+               <a href="package.php" class="btn">discover more</a>
+            </div>
+         </div>
+
+         <div class="swiper-slide slide" style="background:url(images/janter-manter-slide-3.jpg) no-repeat">
+            <div class="content">
+               <span>explore, discover, travel</span>
+               <h3>make your tour worthwhile</h3>
+               <a href="package.php" class="btn">discover more</a>
+            </div>
+         </div>
+         
+      </div>
+
+      <div class="swiper-button-next"></div>
+      <div class="swiper-button-prev"></div>
+
+   </div>
+
+</section>
+<!-- home section ends -->
+<!-- services section starts  -->
+<section class="services">
+   <h1 class="heading-title"> our services </h1>
+   <div class="box-container">
+      <div class="box">
+         <img src="images/icon-2.png" alt="">
+         <h3>tour guide</h3>
+      </div>
+      <div class="box">
+         <img src="images/icon-6.png" alt="">
+         <h3>camping</h3>
+      </div>
+      <div class="box">
+         <img src="images/icon-1.png" alt="">
+         <h3>adventure</h3>
+      </div>
+      <div class="box">
+         <img src="images/icon-3.png" alt="">
+         <h3>trekking</h3>
+      </div>
+      <div class="box">
+         <img src="images/icon-5.png" alt="">
+         <h3>off road</h3>
+      </div>
+      <div class="box">
+         <img src="images/icon-4.png" alt="">
+         <h3>camp fire</h3>
+      </div>
+   </div>
+</section>
+<!-- services section ends -->
+<!-- home about section starts  -->
+<section class="home-about">
+   <div class="image">
+      <img src="images/about-img.jpg" alt="">
+   </div>
+   <div class="content">
+      <h3>about us</h3>
+      <p>Our TOUR BUDDY Is A Website That Provides Trip Fares, Or A Combination Of Both. It Gives Brief About The Famous Tourist Places Of INDIA.We provide Convenience to user During his/her travel.</p>
+      <a href="about.php" class="btn">read more</a>
+   </div>
+</section>
+<!-- home about section ends -->
+<!-- home packages section starts  -->
+<section class="home-packages">
+
+   <h1 class="heading-title"> our packages </h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-1.jpg" alt="Tajmahal">
+         </div>
+         <div class="content">
+            <h3>Tajmahal(Agra)</h3>
+            <p>A landmark monument and a UNESCO World Heritage Site, the Taj Mahal is one of the most admired Mughal masterpieces in the world and is nothing short of poetry crafted in marble.Distance:550KM</p>
+            <div style="font-size: 20px;cursor:pointer;">
+               <span>₹2000 </span>
+            </div>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/janter-manter.jpg" alt="janter-manter">
+         </div>
+         <div class="content">
+            <h3>Jantar-Mantar(Jaipur)</h3>
+            <p>The Jantar Mantar, Jaipur is a collection of 19 astronomical instruments built by the Rajput king Sawai Jai Singh II, the founder of Jaipur, Rajasthan.</p>
+            <div style="font-size: 20px;cursor:pointer;">
+               <span>₹8000 </span>
+            </div>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+      
+      <div class="box">
+         <div class="image">
+            <img src="images/jal-mahal.jpg" alt="jalmahal" >
+         </div>
+         <div class="content">
+            <h3>JalMahal(Udaipur)</h3>
+            <p>The Jal Mahal palace is an architectural showcase of the Rajput style of architecture (common in Rajasthan) on a grand scale. <br> <br></p>
+            <div style="font-size: 20px; cursor:pointer">
+               <span>₹2000 </span>
+            </div>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+   </div>
+
+   <div class="load-more"> <a href="package.php" class="btn">load more</a> </div>
+
+</section>
+
+<!-- home packages section ends -->
+
+
+
+
+
+
+<button type="button" class="scroll-top"><i class="fa fa-angle-double-up" aria-hidden="true"></i></button>
+<!-- footer section starts  -->
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+         <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+      </div>
+
+ 
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> 9999999999 </a>
+         <a href="#"> <i class="fas fa-phone"></i> 1234567890 </a>
+         <a href="#"> <i class="fas fa-phone"></i> 7247251454 </a>
+         <a href="#"> <i class="fas fa-envelope"></i>tourbuddy@gmail.com</a>
+         
+      </div>
+
+      
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#" target="_blank"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#" target="_blank"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="https://www.linkedin.com/in/sujeet-gupta-27bb84223/" target="_blank"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <div class="credit"> created by <span>sujeet</span>  </div>
+
+</section>
+
+<!-- footer section ends -->
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+</body>
+</html>
